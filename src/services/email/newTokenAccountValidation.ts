8d48b485ed96e29151edc42bb0d785585109ca1b
Doriane <berdalado@gmail.com>
2021-11-26 18:00:40 +0100

    resolve conflicts, mutations are working

--- src/services/email/newTokenAccountValidation.ts
+++ src/services/email/newTokenAccountValidation.ts
@@ -1,30 +1,29 @@
 import { User } from "../../models/User";
-const nodemailer = require('nodemailer');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
+export async function sendRefreshToken(token: string, user: User) {
+  
+  // TODO : Send Email - Check if DEV or PROD later
+  const transporter = nodemailer.createTransport({
+    host: "localhost",
+    port: 1025,
+    secure: false,
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false,
+    },
+  });
 
-export async function sendRefreshToken (token: string, user: User) {
+  const url = `${process.env.FRONT_URI_VALIDATE_ACCOUNT}${token}`;
 
-    // TODO : Send Email - Check if DEV or PROD later
-    const transporter = nodemailer.createTransport({
-        host: 'localhost',
-        port: 1025,
-        secure: false,
-        tls: {
-            // do not fail on invalid certs
-            rejectUnauthorized: false,
-        },
-    })
-    
-    const url = `${process.env.FRONT_URI_VALIDATE_ACCOUNT}${token}`
+  var mailOptions = {
+    from: process.env.ADMIN_EMAIL,
+    to: `${user.email}`,
+    subject: `Please take in account your new token ${user.pseudo}`,
+    text: `Here is your new token validation link : ${url}`,
+  };
 
-    var mailOptions = {
-        from: process.env.ADMIN_EMAIL,
-        to: `${user.email}`,
-        subject: `Please take in account your new token ${user.pseudo}`,
-        text: `Here is your new token validation link : ${url}`
-    };
-
-    return await transporter.sendMail(mailOptions);
+  return await transporter.sendMail(mailOptions);
 }
