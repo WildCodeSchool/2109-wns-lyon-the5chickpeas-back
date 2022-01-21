cf0c97c96e0874b67951da8d619a57c41cdb5dc4
Bachir-Ndiaye <amadou.n-diaye@hotmail.com>
2022-01-21 11:49:37 +0100

    add a basic test

---
+++ tests/integration-tests/user.spec.ts
@@ -0,0 +1,7 @@
+import { UsersResolver } from '../../src/resolvers/User';
+
+describe('Add a new user', () => {
+    test('return a valid response', async () => {
+        expect(true).toEqual(true);
+    });
+})
