6950fb14520f27cd01fab2ac3de5ed7c1915b821
HENG Kaniha <hengkaniha@gmail.com>
2022-02-21 11:16:58 +0100

    modify docker-compose ormconfig

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const main = async () => {
     type: "mysql",
     host: "db",
     port: 3306,
-    username: process.env.MYSQL_USERNAME,
+    username: "root",
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     entities: [__dirname + "/models/*.ts"],
